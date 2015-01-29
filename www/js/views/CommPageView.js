80c43033cdae6bf84775348dc2e534fa87a1117e
System Administrator <root@seans-mac-mini.attlocal.net>
2015-01-29 14:03:56 -0800

    Initial commit

---
+++ www/js/views/CommPageView.js
@@ -0,0 +1,84 @@
+/**
+ CommPageView Backbone > View
+ @class CommPageView
+ @constructor
+ @return {Object} instantiated CommPageView
+ **/
+define(['jquery', 'backbone', 'PageView', 'AuthCollection', 'NoteModel'], function ($, Backbone, PageView, AuthCollection, NoteModel) {
+
+    var CommPageView = PageView.extend({
+
+        location: "/pages/CommPage.html",
+        pageID: "CommPage",
+
+        /**
+         Init called from PageView base class
+         @method _initialize
+         **/
+        _initialize: function () {
+            var self = this;
+            self._listenSendPong();
+            self._initModelsCollection();
+        },
+
+        /**
+         Create all the models and collections that we use to communicate with other pages and to server
+         @method _initModelsCollection
+         **/
+        _initModelsCollection: function() {
+            var self = this;
+            self.myNotes1 = new AuthCollection([], {locationUrl: '/cat'});
+            var note = new NoteModel();
+            self.myNotes1.add(note);
+            note.save();
+            self.myNotes1.fetch();
+
+            self.myNotes2 = new AuthCollection([], {locationUrl: '/dog'});
+            var note = new Backbone.Model({'foo': 'bar3'});
+            self.myNotes2.add(note);
+            note.save();
+            self.myNotes2.fetch();
+
+            self.myNotes3 = new AuthCollection([], {locationUrl: '/lion'});
+            var note = new Backbone.Model({'foo': 'bar4'});
+            self.myNotes3.add(note);
+            note.save();
+
+            $(BB.Elements.FIELD1).on('blur', function (e) {
+                var val = $(this).val();
+                self.myNotes1.at(0).set('foo', val);
+                self.myNotes1.at(0).save();
+            });
+
+            $(BB.Elements.FIELD2).on('blur', function (e) {
+                var val = $(this).val();
+                self.myNotes2.at(0).set('foo', val);
+                self.myNotes2.at(0).save();
+            });
+
+            $(BB.Elements.FIELD3).on('blur', function (e) {
+                var val = $(this).val();
+                self.myNotes3.at(0).set('foo', val);
+                self.myNotes3.at(0).save();
+            });
+        },
+
+        /**
+         Listen to click on pong channel events
+         @method _listenSendPong
+         **/
+        _listenSendPong: function () {
+            var self = this;
+            var unsubscribe = BB.comBroker.listenWebViews('pingpong', function (e, reply) {
+                log(e.fromWebView);
+                log('1'+e.event);
+                log(e.data);
+                reply('echo reply...'); // need to setup listener on other side
+            });
+        }
+    });
+
+    return CommPageView;
+});
+
+
